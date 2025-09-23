fbf321a34a71d63b3da2b5047d5c19aefd3cd790
anujkadam62-beep <anujkadam62@gmail.com>
2025-09-23 21:10:35 +0530

    Add files via upload

---
+++ EduConnect/frontend/firebase/firebase-init.js
@@ -0,0 +1,15 @@
+// Replace with your Firebase project config
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFN4via-vxHQMd0jH8cAccp-crCVITg1M",
+    authDomain: "rural-app-d27af.firebaseapp.com",
+    projectId: "rural-app-d27af",
+    storageBucket: "rural-app-d27af.firebasestorage.app",
+    messagingSenderId: "51910719018",
+    appId: "1:51910719018:web:ee006ac3439d65e0cdf553",
+    measurementId: "G-0TX33PP25R"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
